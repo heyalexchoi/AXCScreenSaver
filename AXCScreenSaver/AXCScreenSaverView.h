f65ae5d55e53416553408d31d3f318aa2bbde792
heyalexchoi <heyalexchoi@gmail.com>
2016-04-05 15:03:34 -0400

    Initial Commit

---
+++ AXCScreenSaver/AXCScreenSaverView.h
@@ -0,0 +1,13 @@
+//
+//  AXCScreenSaverView.h
+//  AXCScreenSaver
+//
+//  Created by Alex Choi on 4/5/16.
+//  Copyright © 2016 CHOI. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface AXCScreenSaverView : ScreenSaverView
+
+@end
